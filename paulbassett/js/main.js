f737530fb83ef72d5aa35be0360c81162d11ede5
sfai99 <zer2804@gmail.com>
2024-08-12 13:18:32 +0900

    240812

--- paulbassett/js/main.js
+++ paulbassett/js/main.js
@@ -9,4 +9,43 @@ $(document).ready(function(){
         //pauseOnHover: true, //마우스호버시 일시정지
         //infinite: false, //무한반복부로
     });
+
+    $('.menu .list .popup').slick({
+        dots: false, //하단 페이지 버튼 (true, false)
+	    arrows: false,  //다음, 이전팝업 (true, false)
+        swipeToSlide: true, //드래그한만큼 슬라이드 움직이기
+        slidesToShow: 6, //한번에 보일 팝업 수
+        responsive: [
+            {
+                breakpoint: 1600, //1600px 이하
+                settings: {
+                  slidesToShow: 5
+                }
+              },
+            {
+              breakpoint: 1300, //1300px 이하
+              settings: {
+                slidesToShow: 4
+              }
+            },
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3
+              }
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 2
+              }
+            },
+            {
+              breakpoint: 375,
+              settings: {
+                slidesToShow: 1
+              }
+            },
+        ]
+    })
 })//$(document).ready
