75c6912e993080d370d9faf0582401959193449b
sfai99 <zer2804@gmail.com>
2024-09-26 11:11:59 +0900

    240926

--- potforlio/js/main.js
+++ potforlio/js/main.js
@@ -8,6 +8,16 @@ $(document).ready(function(){
     
     scrollOverflow: false, /* 컨텐츠가 넘쳐도 스크롤 금지 */
 
-    responsiveWidth: 1024 /* fullpage를 적용시키지 않을 모바일 사이즈 */
+    responsiveWidth: 1024, /* fullpage를 적용시키지 않을 모바일 사이즈 */
+
+    afterLoad: function(origin, destination, direction, trigger){
+			if(destination.index == 3){ /* index가 2면 슬라이드는 세번째 슬라이드입니다. index 수는 0/1/2/3 */
+				//console.log('마지막');
+        $('header').hide();
+			}else{
+        $('header').show();
+      }
+		},
+
   });//fullpage
 })//document.ready
